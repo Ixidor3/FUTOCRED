caf61dba7025204c409e822d2ffb8f2ff6356c65
isidore <isidoreeze@gmail.com>
2026-07-14 13:59:13 +0100

    fix: add ABI locally for Vercel deployment

--- src/config/contract.js
+++ src/config/contract.js
@@ -1,4 +1,4 @@
-import ABI from "../../../out/CredentialRegistry.sol/CredentialRegistry.json";
+import ABI from "../abi/CredentialRegistry.json";
 
 export const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
 export const CONTRACT_ABI = ABI.abi;
