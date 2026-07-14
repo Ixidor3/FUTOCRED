864342c8c0757095efe8ef3b7245280b49d9ae66
isidore <isidoreeze@gmail.com>
2026-07-14 14:15:30 +0100

    fix: hardcode contract address

--- src/config/contract.js
+++ src/config/contract.js
@@ -1,4 +1,4 @@
 import ABI from "../abi/CredentialRegistry.json";
 
-export const CONTRACT_ADDRESS = "0x1e01654DCAf5e382Fd158744334B71A0d62Dd8BF";
+export const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
 export const CONTRACT_ABI = ABI.abi;
